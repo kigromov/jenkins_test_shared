1bf2c3057e6369f10a3c2e98ec720cda0ba4385c
Konstantin Gromov <kigromov@gmail.com>
2021-10-24 15:34:13 +0300

    add stages

--- var/startTests.groovy
+++ var/startTests.groovy
@@ -4,23 +4,24 @@ def call(Map<String, String> params) {
     //def threadsCount = params["threadsCount"] ?: "8"
     //def testList = params["testList"] ?: "*Test"
     //def runId = params["runId"]
+    stages {
+            stage("start test job") {
+            // build job: 'QA/TestJob',
+            //         parameters: [string(name: 'threadsCount', value: threadsCount),
+            //                      string(name: 'runId', value: runId),
+            //                      string(name: 'testList', value: testList)]
+                build job: 'AllureTest/allure', 
+                        parameters: [string(name: 'testList', value: '*')]
+            }
 
-    stage("start test job") {
-       // build job: 'QA/TestJob',
-       //         parameters: [string(name: 'threadsCount', value: threadsCount),
-       //                      string(name: 'runId', value: runId),
-       //                      string(name: 'testList', value: testList)]
-        build job: 'AllureTest/allure', 
-                parameters: [string(name: 'testList', value: '*')]
-    }
-
-    stage("get failed test") {
-        def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-        def failedTests = testResultAction.getFailedTests()
-        def failedTestsString = "```"
-        for(CaseResult cr : failedTests) {
-            failedTestsString = failedTestsString + "${cr.getFullDisplayName()}:\n${cr.getErrorDetails()}\n\n"
-        }
-        echo failedTestsString
-    }
+            stage("get failed test") {
+                def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+                def failedTests = testResultAction.getFailedTests()
+                def failedTestsString = "```"
+                for(CaseResult cr : failedTests) {
+                    failedTestsString = failedTestsString + "${cr.getFullDisplayName()}:\n${cr.getErrorDetails()}\n\n"
+                }
+                echo failedTestsString
+            }
+         }
 }
