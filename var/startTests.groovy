a3944c2c4d380c8d5f9f65aba16887cee3ce06ef
Konstantin Gromov <kigromov@gmail.com>
2021-10-24 15:14:50 +0300

    add start test
    add start test

--- var/startTests.groovy
+++ var/startTests.groovy
@@ -1,14 +1,29 @@
+#!groovy
+import hudson.tasks.test.AbstractTestResultAction
+import hudson.tasks.junit.CaseResult
 def call(Map<String, String> params) {
-    assert params["runId"]
+    //assert params["runId"]
 
-    def threadsCount = params["threadsCount"] ?: "8"
-    def testList = params["testList"] ?: "*Test"
-    def runId = params["runId"]
+    //def threadsCount = params["threadsCount"] ?: "8"
+    //def testList = params["testList"] ?: "*Test"
+    //def runId = params["runId"]
 
     stage("start test job") {
-        build job: 'QA/TestJob',
-                parameters: [string(name: 'threadsCount', value: threadsCount),
-                             string(name: 'runId', value: runId),
-                             string(name: 'testList', value: testList)]
+       // build job: 'QA/TestJob',
+       //         parameters: [string(name: 'threadsCount', value: threadsCount),
+       //                      string(name: 'runId', value: runId),
+       //                      string(name: 'testList', value: testList)]
+        build job: 'AllureTest/allure', 
+                parameters: [string(name: 'testList', value: '*')]
+    }
+
+    stage("get failed test") {
+        def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+        def failedTests = testResultAction.getFailedTests()
+        def failedTestsString = "```"
+        for(CaseResult cr : failedTests) {
+            failedTestsString = failedTestsString + "${cr.getFullDisplayName()}:\n${cr.getErrorDetails()}\n\n"
+        }
+        echo failedTestsString
     }
 }
