8d9c9b19689577d803b8a633620af08ba7f62215
Konstantin Gromov <kigromov@gmail.com>
2021-10-24 16:16:46 +0300

    step

--- var/startTests.groovy
+++ var/startTests.groovy
@@ -1,27 +1,7 @@
 #!/usr/bin/env groovy
 def call() {
-    //assert params["runId"]
-
-    //def threadsCount = params["threadsCount"] ?: "8"
-    //def testList = params["testList"] ?: "*Test"
-    //def runId = params["runId"]
-            stage("start test job") {
-            // build job: 'QA/TestJob',
-            //         parameters: [string(name: 'threadsCount', value: threadsCount),
-            //                      string(name: 'runId', value: runId),
-            //                      string(name: 'testList', value: testList)]
-                build job: 'AllureTest/allure', 
-                        parameters: [string(name: 'testList', value: '*')]
-            }
-
-            // stage("get failed test") {
-            //     def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-            //     def failedTests = testResultAction.getFailedTests()
-            //     def failedTestsString = "```"
-            //     for(CaseResult cr : failedTests) {
-            //         failedTestsString = failedTestsString + "${cr.getFullDisplayName()}:\n${cr.getErrorDetails()}\n\n"
-            //     }
-            //     echo failedTestsString
-            // }
-
+    step("start test job") 
+    {
+        build job: 'AllureTest/allure', parameters: [string(name: 'testList', value: '*')]
+    }
 }
